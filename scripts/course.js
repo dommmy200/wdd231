8fbea10610d669dadc32cc8c155ebf501b125ead
Abah Dominic <dabah1@byupathway.edu>
2025-07-05 07:09:21 +0100

    Added more code to course.js

--- scripts/course.js
+++ scripts/course.js
@@ -77,3 +77,87 @@ const courses = [
         completed: false
     }
 ]
+
+function getCourses() {
+    return courses;
+}
+function createAndAppend(total, output) {
+    let totalCreditsOutput = document.createElement('p');
+    totalCreditsOutput.classList.add('total-credits');
+    totalCreditsOutput.textContent = `Total Credits: ${total}`;
+    output.appendChild(totalCreditsOutput);
+}
+let allBtn = document.querySelectorAll('.course-buttons .course-button');
+allBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let totalCredits = 0;
+        
+        if (btn.id === 'all') {
+            let checkmark = '✓';
+            let coursesOutput = document.querySelector('#courses');
+            let courses = getCourses();
+            coursesOutput.innerHTML = ''; // Clear previous output
+            let courseCardContainer = document.createElement('div');
+            courseCardContainer.classList.add('cards-container');
+            
+            courses.forEach(course => {
+                totalCredits += course.credits;
+                let subjNum = `${course.subject} ${course.number}`;
+                let courseCard = document.createElement('span');
+                courseCard.classList.add('course-card');
+                if (course.completed) {
+                    let textNode = document.createTextNode(`${checkmark} ${subjNum}`);
+                    courseCard.appendChild(textNode);
+                    courseCardContainer.appendChild(courseCard);
+                }
+                else {
+                    let textNode = document.createTextNode(`${subjNum}`);
+                    courseCard.appendChild(textNode);
+                    courseCardContainer.appendChild(courseCard);
+                }
+            });
+            coursesOutput.innerHTML = courseCardContainer.outerHTML; // Append all course cards at once
+            createAndAppend(totalCredits, coursesOutput);
+        } else if (btn.id === 'cse') {
+            let checkmark = '✓';
+            let coursesOutput = document.querySelector('#courses');
+            let courses = getCourses().filter(course => course.subject === 'CSE');
+            coursesOutput.innerHTML = ''; // Clear previous output
+            courses.forEach(course => {
+                totalCredits += course.credits;
+                let subjNum = `${course.subject} ${course.number}`;
+                let courseCard = document.createElement('span');
+                courseCard.classList.add('course-card');
+                if (course.completed) {
+                    courseCard.textContent = `${checkmark} ${subjNum}`;
+                    coursesOutput.appendChild(courseCard);
+                }
+                else {
+                    courseCard.textContent = `${subjNum}`;
+                    coursesOutput.appendChild(courseCard);
+                }
+            });
+            createAndAppend(totalCredits, coursesOutput);
+        } else if (btn.id === 'wdd') {
+            let checkmark = '✓';
+            let coursesOutput = document.querySelector('#courses');
+            let courses = getCourses().filter(course => course.subject === 'WDD');
+            coursesOutput.innerHTML = ''; // Clear previous output
+            courses.forEach(course => {
+                totalCredits += course.credits;
+                let subjNum = `${course.subject} ${course.number}`;
+                let courseCard = document.createElement('span');
+                courseCard.classList.add('course-card');
+                if (course.completed) {
+                    courseCard.textContent = `${checkmark} ${subjNum}`;
+                    coursesOutput.appendChild(courseCard);
+                }
+                else {
+                    courseCard.textContent = `${subjNum}`;
+                    coursesOutput.appendChild(courseCard);
+                }
+            });
+            createAndAppend(totalCredits, coursesOutput);
+        }
+    })
+});
